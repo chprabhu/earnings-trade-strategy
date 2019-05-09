7436707d664ab194119c1bd5a89e4cf3448378b8
chprabhu <chprabhu@users.noreply.github.com>
2019-05-09 16:57:35 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,14 +1,12 @@
 ## If you are going to run this, please use your own tokens and API keys
 ## Note, it costs money to pull data from IEX
 
-#os.environ["IEX_API_VERSION"] = "iexcloud-beta"
-#os.environ["IEX_TOKEN"] = "{token}"
-
+os.environ["IEX_API_VERSION"] = "iexcloud-beta"
+os.environ["IEX_TOKEN"] = "{token}"
 
 stock_price_data_path = "stock_returns/"
 earnings_dates_data_path = "stock_returns/earnings_dates/"
 apikey = "{token}"
 num_days = 5
 
-##os.environ["IEX_API_VERSION"] = "iexcloud-sandbox"
-##os.environ["IEX_TOKEN"] = "{token}"
+
